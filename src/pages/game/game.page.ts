27c5d9cac7b6d0e3d91b12a83c86767449483a56
Chacko <chackop@gmail.com>
2017-10-23 01:09:00 +0100

    lodash and game page

--- src/pages/game/game.page.ts
+++ src/pages/game/game.page.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-// import { MapPage, TeamHomePage } from '../pages';
-// import { EliteApi } from '../../shared/shared';
+import {
+  // MapPage, 
+  TeamHomePage
+} from '../pages';
+
+import { EliteApi } from '../../shared/shared';
 // declare var window: any;
 
 @Component({
@@ -14,19 +18,19 @@ export class GamePage {
   constructor(
     public nav: NavController,
     public navParams: NavParams,
-    // public eliteApi: EliteApi
+    public eliteApi: EliteApi
   ) { }
 
-  ionViewDidLoad(){
+  ionViewDidLoad() {
     this.game = this.navParams.data;
     this.game.gameTime = Date.parse(this.game.time);
   }
 
-  // teamTapped(teamId){
-  //   let tourneyData = this.eliteApi.getCurrentTourney();
-  //   let team = tourneyData.teams.find(t => t.id === teamId);
-  //   this.nav.push(TeamHomePage, team); 
-  // }
+  teamTapped(teamId) {
+    let tourneyData = this.eliteApi.getCurrentTourney();
+    let team = tourneyData.teams.find(t => t.id === teamId);
+    this.nav.push(TeamHomePage, team);
+  }
 
   // goToDirections(){
   //   let tourneyData = this.eliteApi.getCurrentTourney();
@@ -38,7 +42,7 @@ export class GamePage {
   //   this.nav.push(MapPage, this.game);
   // }
 
-  isWinner(score1, score2){
+  isWinner(score1, score2) {
     //return Number(score1) > Number(score2);
     return Number(score1) > Number(score2) ? 'secondary' : '';
   }
--- src/pages/team-detail/team-detail.page.ts
+++ src/pages/team-detail/team-detail.page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { AlertController, NavController, NavParams, ToastController } from 'ionic-angular';
 
-// import _ from 'lodash';
+import _ from 'lodash';
 // import moment from 'moment';
 
-// import { GamePage } from '../pages';
-// import { EliteApi, UserSettings } from '../../shared/shared';
+import { GamePage } from '../pages';
+import {
+  EliteApi
+  // , UserSettings
+} from '../../shared/shared';
 
 @Component({
   templateUrl: 'team-detail.page.html',
@@ -22,57 +25,57 @@ export class TeamDetailPage {
 
   constructor(
     public alertController: AlertController,
-    public nav: NavController, 
+    public nav: NavController,
     public navParams: NavParams,
     public toastController: ToastController,
-    // public eliteApi: EliteApi,
+    public eliteApi: EliteApi
     // public userSettings: UserSettings
   ) { }
 
-  ionViewDidLoad(){
-    // this.team = this.navParams.data;
-    // this.tourneyData = this.eliteApi.getCurrentTourney();
+  ionViewDidLoad() {
+    this.team = this.navParams.data;
+    this.tourneyData = this.eliteApi.getCurrentTourney();
 
-    // this.games = _.chain(this.tourneyData.games)
-    //               .filter(g => g.team1Id === this.team.id || g.team2Id === this.team.id)
-    //               .map(g => {
-    //                   let isTeam1 = (g.team1Id === this.team.id);
-    //                   let opponentName = isTeam1 ? g.team2 : g.team1;
-    //                   let scoreDisplay = this.getScoreDisplay(isTeam1, g.team1Score, g.team2Score);
-    //                   return {
-    //                       gameId: g.id,
-    //                       opponent: opponentName,
-    //                       time: Date.parse(g.time),
-    //                       location: g.location,
-    //                       locationUrl: g.locationUrl,
-    //                       scoreDisplay: scoreDisplay,
-    //                       homeAway: (isTeam1 ? "vs." : "at")
-    //                   };
-    //               })
-    //               .value();
-    
+    this.games = _.chain(this.tourneyData.games)
+      .filter(g => g.team1Id === this.team.id || g.team2Id === this.team.id)
+      .map(g => {
+        let isTeam1 = (g.team1Id === this.team.id);
+        let opponentName = isTeam1 ? g.team2 : g.team1;
+        let scoreDisplay = this.getScoreDisplay(isTeam1, g.team1Score, g.team2Score);
+        return {
+          gameId: g.id,
+          opponent: opponentName,
+          time: Date.parse(g.time),
+          location: g.location,
+          locationUrl: g.locationUrl,
+          scoreDisplay: scoreDisplay,
+          homeAway: (isTeam1 ? "vs." : "at")
+        };
+      })
+      .value();
+
     // this.allGames = this.games;
     // this.teamStanding = _.find(this.tourneyData.standings, { 'teamId': this.team.id });
     // this.userSettings.isFavoriteTeam(this.team.id).then(value => this.isFollowing = value);
 
   }
 
-  // getScoreDisplay(isTeam1, team1Score, team2Score) {
-  //       if (team1Score && team2Score) {
-  //           var teamScore = (isTeam1 ? team1Score : team2Score);
-  //           var opponentScore = (isTeam1 ? team2Score : team1Score);
-  //           var winIndicator = teamScore > opponentScore ? "W: " : "L: ";
-  //           return winIndicator + teamScore + "-" + opponentScore;
-  //       }
-  //       else {
-  //           return "";
-  //       }
-  //   }
-  
-  // gameClicked($event, game){
-  //   let sourceGame = this.tourneyData.games.find(g => g.id === game.gameId);
-  //   this.nav.parent.parent.push(GamePage, sourceGame);
-  // } 
+  getScoreDisplay(isTeam1, team1Score, team2Score) {
+    if (team1Score && team2Score) {
+      var teamScore = (isTeam1 ? team1Score : team2Score);
+      var opponentScore = (isTeam1 ? team2Score : team1Score);
+      var winIndicator = teamScore > opponentScore ? "W: " : "L: ";
+      return winIndicator + teamScore + "-" + opponentScore;
+    }
+    else {
+      return "";
+    }
+  }
+
+  gameClicked($event, game){
+    let sourceGame = this.tourneyData.games.find(g => g.id === game.gameId);
+    this.nav.parent.parent.push(GamePage, sourceGame);
+  } 
 
   // getScoreWorL(game){
   //   return game.scoreDisplay ? game.scoreDisplay[0] : '';
